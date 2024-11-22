78a5246e2ea99816b5aec0155629ff92872edd8e
Sherzodbek <sherzoduralov285@gmail.com>
2024-11-22 11:41:45 +0900

    feat: configure env variables

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NestarBatchService {
   getHello(): string {
-    return 'Hello Batch!';
+    return 'Hello Batch 11!';
   }
 }
